ec3454c825877746055226015c8128a1133001bf
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-02-18 20:37:15 +0100

    Add query params

--- src/routes/map.ts
+++ src/routes/map.ts
@@ -3,7 +3,7 @@ namespace Routes {
     const CAMap = require('../MapGeneration/CAMap');
 
     const map =  function(req:any, res:any, next:any) {
-        const camap = new CAMap(100,100);
+        const camap = new CAMap(req.query.width, req.query.height);
         const map = camap.generateMapLayout();
         res.json({map});
     }
